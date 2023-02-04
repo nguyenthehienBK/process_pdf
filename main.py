7c654452a2f58c1f444ec374e08c03241578ba53
nguyenthehienBK <nguyenthehien272@gmail.com>
2023-02-04 22:26:19 +0700

    Add argument: split_end_page & delete_org

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from PyPDF2 import PdfWriter, PdfReader
 from tqdm import tqdm
 from os import listdir
@@ -6,9 +8,14 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Process PDF file!')
 parser.add_argument('--path', type=str, required=True)
+parser.add_argument('--split_end_page', type=int, default=2)
+parser.add_argument('--delete_org', type=str, default="0")
 args = parser.parse_args()
 
 path_to_root = args.path
+split_end_page = args.split_end_page
+delete_org = args.delete_org
+
 print("\n##################################")
 print("Bat dau xu ly cho cac thu muc nam trong:", path_to_root)
 ls_dir = listdir(path_to_root)
@@ -26,22 +33,23 @@ for directory in tqdm(ls_dir):
             with open(path_pdf, "rb") as infile:
                 reader = PdfReader(infile)
                 pdfObj = reader.pages
-                if len(pdfObj) > 2:
+                if len(pdfObj) > split_end_page:
                     writer_1 = PdfWriter()
-                    writer_1.add_page(pdfObj[0])
-                    writer_1.add_page(pdfObj[1])
+                    for page in pdfObj[:split_end_page]:
+                        writer_1.add_page(page)
                     path_pdf_1 = join(path_to_dir, "GCN.pdf")
                     with open(path_pdf_1, "wb") as GCN:
                         writer_1.write(GCN)
 
                     writer_2 = PdfWriter()
-                    for page in pdfObj[2:]:
+                    for page in pdfObj[split_end_page:]:
                         writer_2.add_page(page)
                     path_pdf_2 = join(path_to_dir, "other.pdf")
                     with open(path_pdf_2, "wb") as other:
                         writer_2.write(other)
-
                     count += 1
+            if delete_org == "1":
+                os.remove(path_pdf)
 
 print(f"Hoan thanh xu ly tach file PDF thanh cong cho {count} thu muc!")
 print("##################################\n")
